a8ec76f6d25550f06c75ca46baed409ca98be022
roopeshvs04 <128831043+roopeshvs04@users.noreply.github.com>
2023-07-17 21:08:55 +0530

    initial commit

---
+++ Quiz Application/src/quiz/application/Login.java
@@ -0,0 +1,71 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener{
+
+    JButton rules, back;
+    JTextField tfname;
+
+    Login() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 500, 400);
+        add(image);
+
+        JLabel heading = new JLabel("Simple Minds");
+        heading.setBounds(650, 60, 200, 45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+
+        JLabel name = new JLabel("Enter your name");
+        name.setBounds(710, 100, 200, 20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
+
+        tfname = new JTextField();
+        tfname.setBounds(635, 150, 200, 25);
+        tfname.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(tfname);
+
+        rules = new JButton("Rules");
+        rules.setBounds(635, 170, 100, 25);
+        rules.setBackground(new Color(30, 144, 254));
+        rules.setForeground(Color.WHITE);
+        rules.addActionListener(this);
+        add(rules);
+
+        back = new JButton("Back");
+        back.setBounds(815, 170, 100, 25);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+
+        
+        setSize(50,100);
+        setLocation(100,200);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == rules) {
+            String name = tfname.getText();
+            setVisible(false);
+            new Rules(name);
+        } else if (ae.getSource() == back) {
+            setVisible(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
